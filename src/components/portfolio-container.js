c9c6af4bdb644780d0e216b5fd27bb6ab431abcc
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2025-09-04 21:02:34 +0200

    Added CONSTRUCTOR with STATE INITIALISATION to portfolio-container.js

--- src/components/portfolio-container.js
+++ src/components/portfolio-container.js
@@ -1,11 +1,34 @@
 import React,{ Component } from "react";
 
-export default class PortfolioContainer extends
-Component {
+export default class PortfolioContainer extends Component {
+    
+    // Primer CONSTRUCTOR!
+    constructor(props) {
+
+        super(props);
+
+        console.log("[NOTICE] Estamos usando nuestor primer CONSTURCTOR" )
+        console.log("Muestra de cada super(props):", this.props)
+
+        
+        this.state = {
+
+            pageTitle: "Welcome to my Portfolio!",
+
+            data: [
+                { title: "Portfolio Item 1 ..."},
+                { title: "Portfolio Item 2 ..."},
+                { title: "Portfolio Item 3 ..."},
+                { title: "Portfolio Item 4 ..."}
+            ]
+        };
+    }
+
+
     render() {
         return (
             <div>
-                <h2>Los items portfolio, del nuevo componente, irán aquí:</h2>
+                <h2>Los items portfolio CLASS, del nuevo componente, irán aquí:</h2>
             </div>
         );
     }
