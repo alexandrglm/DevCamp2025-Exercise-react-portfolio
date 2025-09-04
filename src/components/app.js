4aae4f4914a7ecee733201ab3fb599b4238d4433
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2025-09-04 19:40:56 +0200

    Start React-Portfolio Exercise - Some PreFixes made

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Alexandr Gomez - React Portfolio</h1>
+        <h2>Devcamp React Course 2025</h2><br></br>
+        <p>Python version: 2.7.x</p>
+        <p>Node version: 12.13.0</p>
+      </div>
+    );
+  }
+}
