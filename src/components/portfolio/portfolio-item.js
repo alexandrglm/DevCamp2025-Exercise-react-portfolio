044c3ce972628b768097451514a122f00a24ebbe
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2025-09-06 22:26:54 +0200

    08-061: Add dynamic hover effects with event listeners and CSS filter for image darkening

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -39,67 +39,68 @@ y modificamos para crear los links dinámicos de Portfolio
 
 De momento seguimos dejando los debugs de otrsa guías, pero esto cambiará.
 */
-import React from "react";
+import React, { Component } from "react";
 import { Link } from 'react-router-dom'
 
 
-export default function(props) {
 
-    console.log("[NOTICE] --> Props recibidos:", props)
+/* De 08-061, EventListeners, volvemos a pasar el componente a tipo clase*/
+export default class PortfolioItem extends Component {
 
-    /*
-    Valores documentados usando Object.keys(item) con debugger
-        [
-        "id",
-        "name",
-        "description",
-        "url",
-        "category",
-        "position",
-        "thumb_image_url",
-        "banner_image_url",
-        "logo_url",
-        "column_names_merged_with_images"
-        ]
-    */
-    const {
-        id,
-        description,
-        thumb_image_url,
-        logo_url
-    } = props.item
+    constructor(props){
 
+        super(props)
 
+        this.state = {
+
+            portfolioItemClass: ""
+        }
+    }
+
+    handleMouseEnter() {
+
+        this.setState( {
+
+            portfolioItemClass: 'image-blur'
+        })
+
+    }
+
+    handleMouseLeave() {
+
+        this.setState( {
+        
+            portfolioItemClass: ""
+        })
+
+    }
+    
+
+    render() {
+    const { id, description, thumb_image_url, logo_url } = this.props.item;
     return (
+      <div
+        className="portfolio-item-wrapper"
+        onMouseEnter={() => this.handleMouseEnter()}
+        onMouseLeave={() => this.handleMouseLeave()}
+      >
+        <div
+          className={
+            "portfolio-img-background " + this.state.portfolioItemClass
+          }
+          style={{
+            backgroundImage: "url(" + thumb_image_url + ")"
+          }}
+        />
 
-        /* De 08-059*/
-        <div className="portfolio-item-wrapper">
+        <div className="img-text-wrapper">
+          <div className="logo-wrapper">
+            <img src={logo_url} />
+          </div>
 
-            <div className="portfolio-img-background" 
-                style={{ backgroundImage: `url(${thumb_image_url})` }} /> {/* No era mejor usar backtips? */ }
-            
-            {/* De 08-060:  overlaying img, text, overr bg img */}
-            <div className="img-text-wrapper">
-
-                <div className="logo-wrapper">
-                    
-                    <img src={logo_url} /> 
-                
-                </div>
-            
-            </div>
-
-            <div className="subtitle">
-                {description}
-            </div>
-            
-
-            {/* De 08-060   Removing direct link */}
-            <Link to={`/portfolio/${id}`}></Link>
-            {/* props.item.name */ }
-
+          <div className="subtitle">{description}</div>
         </div>
-            
-
+      </div>
     );
+  }
 }
