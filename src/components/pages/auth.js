e01994fc9e80890f5ae64a3afc8fc9120c30aac3
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2025-09-07 13:37:55 +0200

    08-064, Add static image import system with login background image

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,13 +1,34 @@
 // De 08-063, Authentification Component
 import React, { Component } from "react";
 
+/* Ok, los static files podemos importarlos tal cual en imports */
+import loginImg from '../../../static/assets/images/auth/login.jpg'
+
 export default class Auth extends Component {
 
 
     render() {
         
-        return <div> Authentification Page ...</div>
+        return (
+        
+            <div className="auth-page-wrapper">
+                
+                <div 
+                className="left-column" 
+                style={{ 
+                    backgroundImage: `url({$loginImg})`,
+                    height: "200px"
+                 }}
+                
+                >
+                </div>
+                
+                <div className="right-column">
+
+                    <h1>Login Componenet</h1>
+
+                </div>
+            </div>
+        )
     }
-
-
 }
