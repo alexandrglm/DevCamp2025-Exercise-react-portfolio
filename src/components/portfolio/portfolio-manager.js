ed09f041c407f04fc9fb75af0e11bbe313a54bf3
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2025-09-08 16:13:44 +0200

    08-083, Add API call integration to PortfolioManager with state and LIFECYLE HOOKS, response well-structured with ...spread

--- src/components/portfolio/portfolio-manager.js
+++ src/components/portfolio/portfolio-manager.js
@@ -1,7 +1,49 @@
 // de 08-081, SecureClass Component
 import React, { Component } from "react";
+import axios from 'axios'
 
 export default class PortfolioManager extends Component {
+
+
+    //de 08-083, cosntructor para portfolio-manager con state inicial
+    constructor(){
+
+        super();
+
+        this.state = {
+
+            portfolioItems: []
+        }
+
+    }
+    getPortfolioItems() {
+
+        axios
+        .get('https://apialexandr.devcamp.space/portfolio/portfolio_items', { withCredential: true  } )
+        .then( response => {
+
+            console.log('[AXIOS GET PORTFOLIO-ITEMS] Response: ', response )
+            
+            this.setState( {
+
+
+                // !!! con SPREAD obtenemos un array LIMPIO, sin spread obtenemos arrays dentro de arrays por objteo
+                portfolioItems: [...response.data.portfolio_items]
+            })
+
+                })
+        .catch( error => {
+
+            console.log('AXIOS ERROR] Error: ', error)
+
+        })
+    }
+
+    componentDidMount() {
+
+        this.getPortfolioItems()
+    }
+
 
     render() {
 
