cd5372c545bd8b5ad8dd7324a87f72b41e49b693
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2025-09-12 17:05:32 +0200

    09-124 -> Add single post API calls

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,8 +1,42 @@
+import Axios from "axios";
 import React, { Component } from "react";
+
+const miApi = 'https://apialexandr.devcamp.space'
 
 export default class BlogDetail extends Component {
 
+    constructor(props){
 
+        super(props)
+
+        this.state = {
+
+            currentId: this.props.match.params.slug,
+            blogItem: {}
+
+        }
+
+        this.getBlogItem = this.getBlogItem.bind(this)
+    }
+
+    getBlogItem(){
+
+        axios 
+            .get(`${miApi}/portfolio/portfolio_blogs/${this.state.currentId}`)
+            .then( response =>{
+
+                console.log(response)
+
+            })
+            .catch( error =>{
+                console.log(error)
+            })
+
+    }
+
+    componentDidMount(){
+        this.getBlogItem()
+    }
 
     render(){
 
