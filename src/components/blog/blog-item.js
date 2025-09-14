d24517eefb4e65b157832b816ba9a859956d4f69
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2025-09-14 21:52:25 +0200

    10-156 -> Add Truncate & striptags to blog-item component (Add link 'readmore'-alike and re-format content)

--- src/components/blog/blog-item.js
+++ src/components/blog/blog-item.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from 'react-router-dom'
 // De 10-154
 import ReactHtmlParser from 'react-html-parser'
+// De 10-156
+import striptags from 'striptags'
+import Truncate from 'react-truncate'
 
 const BlogItem = props =>{
 
@@ -24,7 +27,25 @@ const BlogItem = props =>{
                 <h1>{title}</h1>
             </Link>
 
-            <div>{ReactHtmlParser(content)}</div>
+            {/* <div>{ReactHtmlParser(content)}</div> */}
+            <div>
+
+                <Truncate
+                    lines={5}
+                    ellipsis={
+
+                        <span>
+                            ...<Link to={`/b/${id}`}>Read More</Link>
+                        </span>
+                    }
+                >
+
+                    {striptags(content)}
+
+                </Truncate>
+
+            </div>
+
 
         </div>
 
